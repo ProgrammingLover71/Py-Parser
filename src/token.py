426cacbd5bbed1bd88db22645244a2c0aa10b81a
ProgrammingLover71 <87024180+ProgrammingLover71@users.noreply.github.com>
2025-07-04 08:54:34 +0300

    Add lexer.py & add Token class

--- src/token.py
+++ src/token.py
@@ -1,3 +1,6 @@
+from enum import Enum
+
+
 class TokenType:
     def __init__(self, name: str, contains_syntax: bool = False):
         self.name = name
@@ -6,5 +9,18 @@ class TokenType:
     def __repr__(self):
         return f'TokenType({self.name})'
     
-    def __call__(self, value):
-        pass
+
+class TokenTypes(Enum):
+    EOF = TokenType('EOF')
+    NUMBER = TokenType('NUMBER', True)
+
+
+class Token:
+    def __init__(self, tok_type: TokenType, line: int, col: int, tok_value=None):
+        self.type = tok_type
+        self.value = tok_value if tok_type.contains_syntax else None
+        self.line = line
+        self.column = col
+    
+    def __repr__(self):
+        return f'Token(type={self.type}, value="{self.value}", pos={self.line}:{self.column})'
